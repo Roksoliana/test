ee8c1a3bd7aa9115ed3922438479443d0a0c471d
Roksoliana <yatsunda7@gmail.com>
2019-01-24 23:54:52 +0200

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return fetch('/data.json')
-      .then(res=>res.json())
-      .then(data=> {data.items = data.items.sort((a,b)=>(a.title>b.title)?1:((a.title<b.title)?-1:0));
+      .then(res => res.json())
+      .then(data => {
+        data.items = data.items
+          .sort((a, b) => a.title > b.title ? 1 : a.title < b.title ? -1 : 0);
         return data;
       });
   }
